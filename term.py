21ddbb5cef9c29fb81419f223abdd9e9793db84d
calacuda <eowest@gmail.com>
2020-11-04 15:47:19 -0500

    testing

--- term.py
+++ term.py
@@ -9,16 +9,18 @@ by: Calacuda | MIT Licence
 
 import pty
 import os
+import re
 
 
 pass_file = "/tmp/julia-voice-programming.txt"
 shell = "julia"
+ansi_escape = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])')
 
 def read(fd):
     data = os.read(fd, 1024)
     #script.write(data)
-    with open(pass_file, 'a') as pf:
-        pf.write(data.decode("utf-8"))
+    with open(pass_file, 'w') as pf:
+        pf.write(ansi_escape.sub('',data.decode("utf-8")))
     return data
 
 
