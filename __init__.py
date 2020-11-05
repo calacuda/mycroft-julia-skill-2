b87eb395cc7bdea34a98c7643a92db33f1691034
calacuda <eowest@gmail.com>
2020-11-05 05:18:59 -0500

    testing

--- __init__.py
+++ __init__.py
@@ -39,7 +39,7 @@ class JuliaVoiceProgramer(MycroftSkill):
         #cmd(f'notify-send "testing" "code :  {code}"')
         #output = Main.eval(code)
         #cmd(f'notify-send "testing" "output :  {output}"')
-        time.sleep(2)
+        #time.sleep(2)
         with open(pass_file, 'r') as pf:
             cmd(f'notify-send "testing" "{pf.read()}"')
             self.speak(pf.read())
