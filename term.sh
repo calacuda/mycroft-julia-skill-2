6cf5ce727e9ceec51c8a85d0b53b227eced3ac5b
calacuda <eowest@gmail.com>
2021-02-05 12:29:05 -0500

    misc bug fixes

--- term.sh
+++ term.sh
@@ -4,4 +4,4 @@ TMUX_CALL="tmux -L $1 new-session -s $1 -n $1"
 JULIA_TERM="$2 /opt/mycroft/skills/mycroft-julia-skill-2.calacuda/term.py"
 #JULIA_TERM="$2 term.py"
 #echo "$TERMINAL -e $TMUX_CALL $JULIA_TERM"
-exec "$TERMINAL -e $TMUX_CALL $JULIA_TERM"
+$TERMINAL # -e $TMUX_CALL $JULIA_TERM
